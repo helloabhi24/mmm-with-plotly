12c1c87199c56b6f7d5814786c89d43d42de6a76
Abhishek Kumar <40750418+helloabhi24@users.noreply.github.com>
2018-08-13 22:40:15 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,9 +1,22 @@
 library(plotly)
 library(shiny)
 shinyServer(function(input,output){
-  output$plot1<-renderPlotly(plot_ly(data11,x=~Year,y=~max1,type='bar',name='max'
-  )%>%add_trace(y=~min1,name="min"
-  )%>%add_trace(y=~mean1,name="mean"
-  )%>%add_trace(y=~median1,name='Median'
-  )%>%layout(yaxis=list(title='Count'),barmode='group'))
+  
+  
+  output$plot2<-renderPlot({
+    g<-tapply(as.matrix(IInd[,input$sel]),as.matrix(IInd[,input$s]),mean,simplify = TRUE)
+    barplot(g)
+  })
+  output$plot1<-renderPlotly({
+    g1<-tapply(as.matrix(IInd[,input$sel]),as.matrix(IInd[,input$s]),max,simplify = TRUE,na.rm=TRUE)
+    g2<-tapply(as.matrix(IInd[,input$sel]),as.matrix(IInd[,input$s]),min,simplify = TRUE,na.rm=TRUE)
+    g3<-tapply(as.matrix(IInd[,input$sel]),as.matrix(IInd[,input$s]),mean,simplify = TRUE,na.rm=TRUE)
+    g4<-tapply(as.matrix(IInd[,input$sel]),as.matrix(IInd[,input$s]),median,simplify = TRUE,na.rm=TRUE)
+plot_ly(data11,x=~Year,y=~g1,type='bar',name='max'
+  )%>%add_trace(y=~g2,name="min"
+  )%>%add_trace(y=~g3,name="mean"
+  )%>%add_trace(y=~g4,name='Median'
+  )%>%layout(yaxis=list(title='Count'),barmode='group')})
 })
+
+
