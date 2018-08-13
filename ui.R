e26cc9b20f1b3cc2d36966897260d594c058ac00
Abhishek Kumar <40750418+helloabhi24@users.noreply.github.com>
2018-08-13 22:42:38 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,6 +1,19 @@
 library(shiny)
 library(plotly)
-shinyUI(fluidPage(
-        
-          plotlyOutput("plot1")
-        ))
+shinyUI(
+  fluidPage(titlePanel(title="Data Analysis"),
+            sidebarLayout(sidebarPanel(
+              selectInput("sel","Select One",choices = colnames(IInd[,c(5:23,25:27,29:53)]))
+            ,
+            selectInput("s","NO Touch",choices = colnames(IInd[,3]) )),
+            mainPanel(h4("MEAN,MEDIAN,MIN AND MAX",align="center"),
+                    plotlyOutput("plot1")
+                      # h1("This is Bar Ploting of Test Data",align="center") ,
+                      # plotOutput("plot2")
+                      #   h1("This is Bar Ploting of Test Data",align="center"),
+                      #tableOutput("Plot3")
+                      
+            )))
+  
+)
+
